e1bcec1705d3a994c1dee491b78c3b0626b9d4e6
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2021-05-08 22:35:05 +0200

    more code

--- lib/src/test/java/org/codecraftlabs/mockito/ServiceTest.java
+++ lib/src/test/java/org/codecraftlabs/mockito/ServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
@@ -21,5 +22,16 @@ public class ServiceTest {
         var service = new Service(databaseMock);
         var check = service.query("* from query");
         assertTrue(check);
+    }
+
+    @Test
+    public void ensureMockitoReturnsTheConfiguredValue() {
+
+        // define return value for method getUniqueId()
+        when(databaseMock.getUniqueId()).thenReturn(43);
+
+        Service service = new Service(databaseMock);
+        // use mock in test....
+        assertEquals(service.toString(), "Using database with id: 43");
     }
 }
